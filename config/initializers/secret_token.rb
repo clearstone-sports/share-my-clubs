9ec269397c1819966f0bdae39fb462d314b416f0
Corey Woodcox <corey.woodcox@gmail.com>
2014-02-22 21:59:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShareMyClubs::Application.config.secret_key_base = '384a91cbfcc1b19b02b4d10badfea2cf37f463bccf274050df8e1c0fb36fd7d450bd113c62db537587edbd79acba6dd3455b4cb0bd216fb170a959fdad705ff8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShareMyClubs::Application.config.session_store :cookie_store, key: '_share-my-clubs_session'
