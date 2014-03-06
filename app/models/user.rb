4bf16126d788fdc959ea9aec96950eeaefd9322a
Corey Woodcox <corey.woodcox@gmail.com>
2014-03-05 22:13:53 -0700

    re-annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,17 @@
 #
 # Table name: users
 #
-#  id                 :integer          not null, primary key
-#  email              :string(255)
-#  encrypted_password :string(255)
-#  status             :string(255)
-#  person_id          :integer
-#  created_at         :datetime
-#  updated_at         :datetime
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  password_digest :string(255)
+#  status          :string(255)
+#  person_id       :integer
+#  created_at      :datetime
+#  updated_at      :datetime
 #
 
 class User < ActiveRecord::Base
   has_secure_password validations: false
+
+  validates :email, presence: true
 end
